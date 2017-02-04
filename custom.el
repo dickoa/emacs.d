34ae9668b98534c6124c3baa8738dc002657a8c9
Ahmadou Dicko <dicko.ahmadou@gmail.com>
2017-02-04 12:07:01 +0100

    Update

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" default)))
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" default)))
  '(ess-R-font-lock-keywords
    (quote
     ((ess-R-fl-keyword:modifiers . t)
@@ -39,7 +39,7 @@
  '(org-export-backends (quote (ascii beamer html latex md odt org)))
  '(package-selected-packages
    (quote
-    (slack elfeed elfeed-goodies elfeed-org monroe gitlab use-package swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline polymode ox-reveal ox-nikola ox-ioslide org-plus-contrib org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ess ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))))
+    (emmet-mode confluence org-trello org-plus-contrib volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack elfeed elfeed-goodies elfeed-org monroe gitlab use-package swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline polymode ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ess ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
