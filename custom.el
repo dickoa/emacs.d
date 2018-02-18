5805d40294193e83974cac04c890f80582e165c4
Ahmadou Dicko <ahmadou.dicko@gmail.com>
2018-02-18 19:21:52 +0000

    Update config (flycheck in ESS instead of bug due to flymake)

--- custom.el
+++ custom.el
@@ -4,11 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" default)))
+   '("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" default))
  '(ess-R-font-lock-keywords
-   (quote
-    ((ess-R-fl-keyword:modifiers . t)
+   '((ess-R-fl-keyword:modifiers . t)
      (ess-R-fl-keyword:fun-defs . t)
      (ess-R-fl-keyword:keywords . t)
      (ess-R-fl-keyword:assign-ops . t)
@@ -18,10 +16,9 @@
      (ess-fl-keyword:operators . t)
      (ess-fl-keyword:delimiters)
      (ess-fl-keyword:=)
-     (ess-R-fl-keyword:F&T . t))))
+     (ess-R-fl-keyword:F&T . t)))
  '(inferior-R-font-lock-keywords
-   (quote
-    ((ess-S-fl-keyword:prompt . t)
+   '((ess-S-fl-keyword:prompt . t)
      (ess-R-fl-keyword:messages . t)
      (ess-R-fl-keyword:modifiers . t)
      (ess-R-fl-keyword:fun-defs . t)
@@ -34,10 +31,9 @@
      (ess-fl-keyword:operators . t)
      (ess-fl-keyword:delimiters)
      (ess-fl-keyword:=)
-     (ess-R-fl-keyword:F&T . t))))
+     (ess-R-fl-keyword:F&T . t)))
  '(inferior-ess-r-font-lock-keywords
-   (quote
-    ((ess-S-fl-keyword:prompt . t)
+   '((ess-S-fl-keyword:prompt . t)
      (ess-R-fl-keyword:messages . t)
      (ess-R-fl-keyword:modifiers . t)
      (ess-R-fl-keyword:fun-defs . t)
@@ -50,14 +46,14 @@
      (ess-fl-keyword:operators . t)
      (ess-fl-keyword:delimiters)
      (ess-fl-keyword:=)
-     (ess-R-fl-keyword:F&T . t))))
- '(org-export-backends (quote (ascii beamer html latex md odt org)))
+     (ess-R-fl-keyword:F&T . t)))
+ '(org-export-backends '(ascii beamer html latex md odt org))
  '(org-trello-current-prefix-keybinding "C-c o")
  '(package-selected-packages
-   (quote
-    (csv-mode material-theme use-package toml-mode no-littering js3-mode indium adaptive-wrap ob-async ob-sql-mode eval-in-repl org-beautify-theme mu4e-alert org-alert all-the-icons-dired org-gcal org-gnome org-jira org-readme skewer-mode tabbar tabbar-ruler emmet-mode confluence org-trello org-plus-contrib volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack monroe gitlab swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline polymode ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ess ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout)))
+   '(flycheck remark-mode csv-mode material-theme use-package toml-mode no-littering js3-mode indium adaptive-wrap ob-async ob-sql-mode eval-in-repl org-beautify-theme mu4e-alert org-alert all-the-icons-dired org-gcal org-gnome org-jira org-readme skewer-mode tabbar tabbar-ruler emmet-mode confluence org-trello org-plus-contrib volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack monroe gitlab swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline polymode ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ess ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))
+ '(polymode-exporter-output-file-format "%s")
  '(tool-bar-mode nil)
- '(tramp-syntax (quote default) nil (tramp)))
+ '(tramp-syntax 'default nil (tramp)))
 
 ;; (custom-set-faces
 ;;  ;; custom-set-faces was added by Custom.
