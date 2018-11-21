c905c339f4638e40484f4ad251b16e1b3e06023b
Ahmadou Dicko <mail@ahmadoudicko.com>
2018-11-21 20:15:17 +0000

    Update ESS smart assign (ESS version > 18.10)

--- custom.el
+++ custom.el
@@ -3,19 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(custom-safe-themes
    '("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" default))
  '(ess-R-font-lock-keywords
    '((ess-R-fl-keyword:modifiers . t)
      (ess-R-fl-keyword:fun-defs . t)
+     (ess-R-fl-keyword:bare-keywords . t)
      (ess-R-fl-keyword:keywords . t)
      (ess-R-fl-keyword:assign-ops . t)
      (ess-R-fl-keyword:constants . t)
+     (ess-fl-keyword:matrix-labels . t)
      (ess-fl-keyword:fun-calls . t)
      (ess-fl-keyword:numbers)
      (ess-fl-keyword:operators . t)
      (ess-fl-keyword:delimiters)
-     (ess-fl-keyword:=)
+     (ess-fl-keyword:= . t)
      (ess-R-fl-keyword:F&T . t)))
  '(ess-roxy-str "#'")
  '(ess-roxy-template-alist
@@ -23,7 +26,6 @@
      ("details" . ".. content for \\details{} ..")
      ("param" . "")
      ("return" . "")
-     ("author" . "Ahmadou H. Dicko")
      ("export" . "")))
  '(inferior-R-font-lock-keywords
    '((ess-S-fl-keyword:prompt . t)
@@ -50,15 +52,16 @@
      (ess-R-fl-keyword:constants . t)
      (ess-fl-keyword:matrix-labels . t)
      (ess-fl-keyword:fun-calls . t)
-     (ess-fl-keyword:numbers)
+     (ess-fl-keyword:numbers . t)
      (ess-fl-keyword:operators . t)
      (ess-fl-keyword:delimiters)
-     (ess-fl-keyword:=)
+     (ess-fl-keyword:= . t)
      (ess-R-fl-keyword:F&T . t)))
  '(org-export-backends '(ascii beamer html latex md odt org))
  '(org-trello-current-prefix-keybinding "C-c o")
  '(package-selected-packages
-   '(exec-path-from-shell flycheck-gometalinter go-autocomplete go-mode json-navigator graphviz-dot-mode ag company-tern prettier-js tern xref-js2 nyan-mode modern-cpp-font-lock company-rtags flycheck-irony ivy-rtags company-irony irony doom-themes counsel-projectile treemacs-evil treemacs treemacs-projectile xterm-color counsel gitlab-ci-mode exwm xelb docker flycheck remark-mode csv-mode material-theme use-package toml-mode no-littering js3-mode indium adaptive-wrap ob-async ob-sql-mode eval-in-repl org-beautify-theme mu4e-alert org-alert all-the-icons-dired org-gcal org-gnome org-jira org-readme skewer-mode tabbar tabbar-ruler emmet-mode confluence org-trello org-plus-contrib volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack monroe gitlab swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline polymode ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ess ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))
+   '(julia-mode poly-R poly-erb poly-noweb poly-org poly-ruby poly-slim polymode poly-markdown ess-view ess yaml-mode openwith cask tablist pdf-tools js2-mode json-mode org-plus-contrib auctex auto-complete exec-path-from-shell flycheck-gometalinter go-autocomplete go-mode json-navigator graphviz-dot-mode ag company-tern prettier-js tern xref-js2 nyan-mode modern-cpp-font-lock company-rtags flycheck-irony ivy-rtags company-irony irony doom-themes counsel-projectile treemacs-evil treemacs treemacs-projectile xterm-color counsel gitlab-ci-mode exwm xelb docker flycheck remark-mode csv-mode material-theme use-package toml-mode no-littering js3-mode indium adaptive-wrap ob-async ob-sql-mode eval-in-repl org-beautify-theme mu4e-alert org-alert all-the-icons-dired org-gcal org-gnome org-jira org-readme skewer-mode tabbar tabbar-ruler emmet-mode confluence org-trello volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack monroe gitlab swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))
+ '(pdf-view-use-imagemagick t)
  '(polymode-exporter-output-file-format "%s")
  '(tool-bar-mode nil)
  '(tramp-syntax 'default nil (tramp)))
