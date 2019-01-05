23fff817bcc982e99ac2d8aa9fedefa1e60d9bd0
Ahmadou Dicko <mail@ahmadoudicko.com>
2019-01-05 16:09:56 +0000

    Update init file

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#5f5f5f" "#ff4b4b" "#a1db00" "#fce94f" "#5fafd7" "#d18aff" "#afd7ff" "#ffffff"])
  '(blink-cursor-mode nil)
- '(custom-safe-themes
-   '("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" default))
  '(ess-R-font-lock-keywords
    '((ess-R-fl-keyword:modifiers . t)
      (ess-R-fl-keyword:fun-defs . t)
@@ -58,20 +58,14 @@
      (ess-fl-keyword:= . t)
      (ess-R-fl-keyword:F&T . t)))
  '(org-export-backends '(ascii beamer html latex md odt org))
- '(org-trello-current-prefix-keybinding "C-c o")
  '(package-selected-packages
-   '(forge company-nginx nginx-mode all-the-icons-ivy dockerfile-mode julia-mode poly-R poly-erb poly-noweb poly-org poly-ruby poly-slim polymode poly-markdown ess-view ess yaml-mode openwith cask tablist pdf-tools js2-mode json-mode org-plus-contrib auctex auto-complete exec-path-from-shell flycheck-gometalinter go-autocomplete go-mode json-navigator graphviz-dot-mode ag company-tern prettier-js tern xref-js2 nyan-mode modern-cpp-font-lock company-rtags flycheck-irony ivy-rtags company-irony irony doom-themes counsel-projectile treemacs-evil treemacs treemacs-projectile xterm-color counsel gitlab-ci-mode exwm xelb docker flycheck remark-mode csv-mode material-theme use-package toml-mode no-littering js3-mode indium adaptive-wrap ob-async ob-sql-mode eval-in-repl org-beautify-theme mu4e-alert org-alert all-the-icons-dired org-gcal org-gnome org-jira org-readme skewer-mode tabbar tabbar-ruler emmet-mode confluence org-trello volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack monroe gitlab swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))
+   '(apropospriate-theme tangotango-theme alect-themes zerodark-theme forge company-nginx nginx-mode all-the-icons-ivy dockerfile-mode julia-mode poly-R poly-erb poly-noweb poly-org poly-ruby poly-slim polymode poly-markdown ess-view ess yaml-mode openwith cask tablist pdf-tools js2-mode json-mode org-plus-contrib auctex auto-complete exec-path-from-shell flycheck-gometalinter go-autocomplete go-mode json-navigator graphviz-dot-mode ag company-tern prettier-js tern xref-js2 nyan-mode modern-cpp-font-lock company-rtags flycheck-irony ivy-rtags company-irony irony doom-themes counsel-projectile treemacs-evil treemacs treemacs-projectile xterm-color counsel gitlab-ci-mode exwm xelb docker flycheck remark-mode csv-mode material-theme use-package toml-mode no-littering js3-mode indium adaptive-wrap ob-async ob-sql-mode eval-in-repl org-beautify-theme mu4e-alert org-alert all-the-icons-dired org-gcal org-gnome org-jira org-readme skewer-mode tabbar tabbar-ruler emmet-mode confluence org-trello volatile-highlights smart-tabs-mode smart-mode-line-powerline-theme ample-theme ob-ipython ein aggressive-indent neotree all-the-icons ivy-bibtex ivy-gitlab python-environment python-mode python-test python-x slack monroe gitlab swiper stan-mode smartparens smart-mode-line scala-mode2 projectile powerline ox-reveal ox-nikola ox-ioslide org-eww org-bullets ob-lisp moe-theme mode-icons markdown-mode magit ido-vertical-mode ido-ubiquitous htmlize hlinum grass-mode flymd eww-lnum ensime elpy cider biblio bbdb auto-complete-auctex auctex-latexmk 0blayout))
  '(pdf-view-use-imagemagick t)
  '(polymode-exporter-output-file-format "%s")
  '(tool-bar-mode nil)
- '(tramp-syntax 'default nil (tramp)))
+ '(tramp-syntax 'default nil (tramp))
+ '(window-divider-default-places t))
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 113 :width normal)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
